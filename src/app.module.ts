e3c8b1a5ab749655a3301b4f04f1f8c83ded51be
UglystoneDeveloper <kimeende@naver.com>
2023-03-30 20:17:51 +0900

    te

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGateway } from './chat.gateway';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'main-sejong.cp9j2b8wvscr.ap-northeast-2.rds.amazonaws.com',
-      port: 3306,
-      username: 'admin',
-      password: 'uglystone1!',
-      database: 'mainsejong',
-      entities: [ChatGateway], // 사용할 entity의 클래스명을 넣어둔다.
-      synchronize: false, // false로 해두는 게 안전하다.
-    }),
+    // TypeOrmModule.forRoot({
+      // type: 'mysql',
+      // host: 'main-sejong.cp9j2b8wvscr.ap-northeast-2.rds.amazonaws.com',
+      // port: 3306,
+      // username: 'admin',
+      // password: 'uglystone1!',
+      // database: 'mainsejong',
+      // entities: [ChatGateway], // 사용할 entity의 클래스명을 넣어둔다.
+      // synchronize: false, // false로 해두는 게 안전하다.
+    // }
+    // ),
   ],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
