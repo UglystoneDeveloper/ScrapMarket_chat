3df1e5e76571a9bf16dbf1553c21895b299b1708
UglystoneDeveloper <kimeende@naver.com>
2023-03-30 17:40:39 +0900

    te

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,12 +1,26 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer , ConnectedSocket  } from "@nestjs/websockets";
+// import { Socket } from "socket.io"; -> 문제
 
-@WebSocketGateway()
+
+
+@WebSocketGateway({
+  cors: {
+  origin: ['http://localhost:8080'],
+},
+})
 export class ChatGateway {
   @WebSocketServer()
   server;
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string , nickname:string): void {
-    this.server.emit('message', message);
+  handleMessage(
+    @MessageBody() message: string,
+    // @ConnectedSocket() client: Socket, -> 문제
+    ): 
+  void {
+    // client.join(message.split("방이름")[1]) -> 문제
+    // console.log(message.split("방이름")[1] + "입장")
+    this.server.to(message.split("방이름")[1]).emit('message', message);
+    // this.server.emit('message', message);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+
+  app.enableCors({
+    origin: /^(https:\/\/([^\.]*\.)?example\.com)$/i,
+  });
+
+    // const corsOptions: CorsOptions = {
+    //   origin: '*',
+    //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    //   allowedHeaders: ['*'],
+    //   exposedHeaders: ['*'],
+    //   credentials: true,
+    // };
+    // app.enableCors(corsOptions);
+  
+    await app.listen(3000);
+
 }
+
 bootstrap();
