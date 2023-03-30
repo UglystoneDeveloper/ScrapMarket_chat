7959622c2e4fba0cd074f655d491e11d0cdfab2e
UglystoneDeveloper <kimeende@naver.com>
2023-03-30 20:26:54 +0900

    te

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGateway } from './chat.gateway';
-// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    // TypeOrmModule.forRoot({
-      // type: 'mysql',
-      // host: 'main-sejong.cp9j2b8wvscr.ap-northeast-2.rds.amazonaws.com',
-      // port: 3306,
-      // username: 'admin',
-      // password: 'uglystone1!',
-      // database: 'mainsejong',
-      // entities: [ChatGateway], // 사용할 entity의 클래스명을 넣어둔다.
-      // synchronize: false, // false로 해두는 게 안전하다.
-    // }
-    // ),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,7 +1,4 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer , ConnectedSocket  } from "@nestjs/websockets";
-// import { Socket } from "socket.io"; -> 문제
-
-
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 
 @WebSocketGateway()
 export class ChatGateway {
@@ -9,14 +6,7 @@ export class ChatGateway {
   server;
 
   @SubscribeMessage('message')
-  handleMessage(
-    @MessageBody() message: string,
-    // @ConnectedSocket() client: Socket, -> 문제
-    ): 
-  void {
-    // client.join(message.split("방이름")[1]) -> 문제
-    // console.log(message.split("방이름")[1] + "입장")
-    this.server.to(message.split("방이름")[1]).emit('message', message);
-    // this.server.emit('message', message);
+  handleMessage(@MessageBody() message: string , nickname:string): void {
+    this.server.emit('message', message);
   }
 }
