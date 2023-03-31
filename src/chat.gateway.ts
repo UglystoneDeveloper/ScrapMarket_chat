86244f731d13e485182ebc6496f812fe7d2d52ed
UglystoneDeveloper <kimeende@naver.com>
2023-03-31 09:02:23 +0900

    채팅부분 마무리

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,12 +1,35 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer , ConnectedSocket  } from "@nestjs/websockets";
+import { Socket } from "socket.io";
 
-@WebSocketGateway()
+
+
+@WebSocketGateway({
+  cors : {
+    origin:'*'
+  }
+})
 export class ChatGateway {
   @WebSocketServer()
   server;
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string , nickname:string): void {
-    this.server.emit('message', message);
+  handleMessage(
+    @MessageBody() message: string,
+    ): 
+  void {
+    this.server.to(message.split("방이름")[1]).emit('message', message);
+  }
+
+  
+  @SubscribeMessage('room')
+  createroom(
+    @MessageBody() room: string,
+    @ConnectedSocket() client: Socket, // -> 문제되는부분
+    ): 
+  void {
+    client.join(room)
+    // console.log(message.split("방이름")[1] + "입장")
+    // this.server.to(message.split("방이름")[1]).emit('message', message);
+    // this.server.emit('message', message);
   }
 }
