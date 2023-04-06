a4e90550e36d1e639bd8d0164e6f2e206c43b4ba
UglystoneDeveloper <kimeende@naver.com>
2023-04-06 22:12:27 +0900

    te

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGateway } from './chat.gateway';
+import { ServeStaticModule } from '@nestjs/serve-static/dist/serve-static.module';
+import { join } from 'path';
 
 @Module({
   imports: [
